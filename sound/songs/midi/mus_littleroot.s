476741090be698070cedb731d61cbb3ff112c0cd
P <wingzro17@hotmail.com>
2026-02-15 00:51:34 -0500

    1

--- sound/songs/midi/mus_littleroot.s
+++ sound/songs/midi/mus_littleroot.s
@@ -1,6 +1,6 @@
 	.include "MPlayDef.s"
 
-	.equ	mus_littleroot_grp, voicegroup_littleroot
+	.equ	mus_littleroot_grp, voicegroup_colosseum_trainer_voicegroup
 	.equ	mus_littleroot_pri, 0
 	.equ	mus_littleroot_rev, reverb_set+50
 	.equ	mus_littleroot_mvl, 100
