03ea247867715c648d63c835227a22d3c55658f6
P <wingzro17@hotmail.com>
2026-02-19 00:29:40 -0500

    Update audio_rules.mk

--- audio_rules.mk
+++ audio_rules.mk
@@ -37,14 +37,19 @@ $(SOUND_BIN_DIR)/%.bin: sound/%.wav
 MID_CFG_PATH := $(MID_SUBDIR)/midi.cfg
 
 # $1: Source path no extension, $2 Options
-define MID_RULE
-$(MID_ASM_DIR)/$1.s: $(MID_SUBDIR)/$1.mid $(MID_CFG_PATH) $(EXPANSION_BATTLE_CONFIG)
-	$(MID) $$< $$@ $2
-endef
+#define MID_RULE
+#$(MID_ASM_DIR)/$1.s: $(MID_SUBDIR)/$1.mid $(MID_CFG_PATH) $(EXPANSION_BATTLE_CONFIG)
+#	$(MID) $$< $$@ $2
+#endef
+
+
 #                            source path,                             remaining text (options)
-define MID_EXPANSION
-	$(eval $(call MID_RULE,$(basename $(patsubst %:,%,$(word 1,$1))),$(wordlist 2,999,$1)))
-endef
+#define MID_EXPANSION
+#	$(eval $(call MID_RULE,$(basename $(patsubst %:,%,$(word 1,$1))),$(wordlist 2,999,$1)))
+#endef
+
+
+
 
 $(foreach line,$(shell cat $(MID_CFG_PATH) | sed "s/ /__SPACE__/g"),$(call MID_EXPANSION,$(subst __SPACE__, ,$(line))))
 
