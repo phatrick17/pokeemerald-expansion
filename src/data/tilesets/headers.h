3e17f0681daf65184dbddf136d4d9176036f5e2e
P <wingzro17@hotmail.com>
2026-03-26 23:24:15 -0400

    Revert "commit"
    This reverts commit 69a9e819bd09fcff46bda0ff17c3644af3e05076.

--- src/data/tilesets/headers.h
+++ src/data/tilesets/headers.h
@@ -872,6 +872,5 @@ const struct Tileset gTileset_Outskirt_Stand_Indoor =
     .palettes = gTilesetPalettes_Outskirt_Stand_Indoor,
     .metatiles = gMetatiles_Outskirt_Stand_Indoor,
     .metatileAttributes = gMetatileAttributes_Outskirt_Stand_Indoor,
-    .callback = NULL,
+    .callback = InitTilesetAnim_OutskirtStandIndoor,
 };
-
