cedb70ac47338903e0ecff53959ba4b060b96e45
P <wingzro17@hotmail.com>
2026-03-28 14:52:55 -0400

    Rider Willie pt 2

--- src/data/graphics/trainers.h
+++ src/data/graphics/trainers.h
@@ -404,7 +404,7 @@ const struct TrainerSprite gTrainerSprites[] =
     TRAINER_SPRITE(TRAINER_PIC_LEAF, gTrainerFrontPic_Leaf, gTrainerPalette_Leaf),
     TRAINER_SPRITE(TRAINER_PIC_RS_BRENDAN, gTrainerFrontPic_RubySapphireBrendan, gTrainerPalette_RubySapphireBrendan),
     TRAINER_SPRITE(TRAINER_PIC_RS_MAY, gTrainerFrontPic_RubySapphireMay, gTrainerPalette_RubySapphireMay),
-    TRAINER_SPRITE(TRAINER_PIC_RIDER_WILLIE, gTrainerFrontPic_Rider_Willie, gTrainerPalette_Rider_Willie),
+    TRAINER_SPRITE(TRAINER_PIC_RIDER_WILLIE, gTrainerFrontPic_RiderWillie, gTrainerPalette_RiderWillie),
 };
 
 static const union AnimCmd sAnimCmd_Hoenn[] =
