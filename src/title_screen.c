f979418ce901f61369075e22801a2cc4e40af113
P <wingzro17@hotmail.com>
2026-04-09 10:19:58 -0400

    Update title_screen.c

--- src/title_screen.c
+++ src/title_screen.c
@@ -27,6 +27,8 @@ enum {
     TAG_VERSION = 1000,
     TAG_PRESS_START_COPYRIGHT,
     TAG_LOGO_SHINE,
+    TAG_ESPEON,
+    TAG_UMBREON,
 };
 
 #define VERSION_BANNER_RIGHT_TILEOFFSET 64
@@ -360,6 +362,67 @@ static const struct CompressedSpriteSheet sPokemonLogoShineSpriteSheet[] =
     {},
 };
 
+static const struct OamData sEspeonUmbreonOamData =
+{
+    .affineMode = ST_OAM_AFFINE_OFF,
+    .objMode = ST_OAM_OBJ_NORMAL,
+    .mosaic = FALSE,
+    .bpp = ST_OAM_8BPP,
+    .shape = SPRITE_SHAPE(64x64),
+    .x = 0,
+    .matrixNum = 0,
+    .size = SPRITE_SIZE(64x64),
+    .tileNum = 0,
+    .priority = 2,
+    .paletteNum = 0,
+    .affineParam = 0,
+};
+ 
+static const union AnimCmd sEspeonAnimSequence[] =
+{
+    ANIMCMD_FRAME(0, 0),
+    ANIMCMD_END,
+};
+ 
+static const union AnimCmd sUmbreonAnimSequence[] =
+{
+    ANIMCMD_FRAME(0, 0),
+    ANIMCMD_END,
+};
+ 
+static const union AnimCmd *const sEspeonAnimTable[] =
+{
+    sEspeonAnimSequence,
+};
+ 
+static const union AnimCmd *const sUmbreonAnimTable[] =
+{
+    sUmbreonAnimSequence,
+};
+ 
+static const struct SpriteTemplate sEspeonSpriteTemplate =
+{
+    .tileTag = TAG_ESPEON,
+    .paletteTag = TAG_ESPEON,
+    .oam = &sEspeonUmbreonOamData,
+    .anims = sEspeonAnimTable,
+    .images = NULL,
+    .affineAnims = gDummySpriteAffineAnimTable,
+    .callback = SpriteCallbackDummy,
+};
+ 
+static const struct SpriteTemplate sUmbreonSpriteTemplate =
+{
+    .tileTag = TAG_UMBREON,
+    .paletteTag = TAG_UMBREON,
+    .oam = &sEspeonUmbreonOamData,
+    .anims = sUmbreonAnimTable,
+    .images = NULL,
+    .affineAnims = gDummySpriteAffineAnimTable,
+    .callback = SpriteCallbackDummy,
+};
+ 
+
 // Task data for the main title screen tasks (Task_TitleScreenPhase#)
 #define tCounter    data[0]
 #define tSkipToNext data[1]
@@ -567,6 +630,23 @@ static void VBlankCB(void)
     SetGpuReg(REG_OFFSET_BG1VOFS, gBattle_BG1_Y);
 }
 
+static void LoadTitleScreenPokemonSprite(u16 species, u16 tag, const struct SpriteTemplate *spriteTemplate, s16 x, s16 y)
+{
+    // Load front battle sprite graphics and palette
+    LoadCompressedSpriteSheet(&(struct CompressedSpriteSheet){
+        .data = gMonFrontPicTable[species],
+        .size = 0x800,
+        .tag = tag
+    });
+ 
+    // Load palette
+    LoadSpritePaletteWithTag(GetMonSpritePalFromSpecies(species, FALSE, FALSE), tag);
+ 
+    // Create the sprite
+    u8 spriteId = CreateSprite(spriteTemplate, x, y, 0);
+}
+
+
 void CB2_InitTitleScreen(void)
 {
     switch (gMain.state)
@@ -619,6 +699,10 @@ void CB2_InitTitleScreen(void)
         break;
     case 2:
     {
+            // Load Espeon and Umbreon sprites
+        LoadTitleScreenPokemonSprite(SPECIES_ESPEON, TAG_ESPEON, &sEspeonSpriteTemplate, 50, 100);
+        LoadTitleScreenPokemonSprite(SPECIES_UMBREON, TAG_UMBREON, &sUmbreonSpriteTemplate, 190, 100);
+ 
         u8 taskId = CreateTask(Task_TitleScreenPhase1, 0);
 
         gTasks[taskId].tCounter = 256;
@@ -645,7 +729,7 @@ void CB2_InitTitleScreen(void)
         SetGpuReg(REG_OFFSET_WIN1V, 0);
         SetGpuReg(REG_OFFSET_WININ, WININ_WIN0_BG_ALL | WININ_WIN0_OBJ | WININ_WIN1_BG_ALL | WININ_WIN1_OBJ);
         SetGpuReg(REG_OFFSET_WINOUT, WINOUT_WIN01_BG_ALL | WINOUT_WIN01_OBJ | WINOUT_WINOBJ_ALL);
-        SetGpuReg(REG_OFFSET_BLDCNT, BLDCNT_TGT1_BG2); //| BLDCNT_EFFECT_LIGHTEN);
+        SetGpuReg(REG_OFFSET_BLDCNT, BLDCNT_TGT1_BG2 | BLDCNT_EFFECT_LIGHTEN);
         SetGpuReg(REG_OFFSET_BLDALPHA, 0);
         SetGpuReg(REG_OFFSET_BLDY, 12);
         SetGpuReg(REG_OFFSET_BG0CNT, BGCNT_PRIORITY(3) | BGCNT_CHARBASE(2) | BGCNT_SCREENBASE(26) | BGCNT_16COLOR | BGCNT_TXT256x256);
@@ -856,14 +940,15 @@ static void CB2_GoToBerryFixScreen(void)
 
 static void UpdateLegendaryMarkingColor(u8 frameNum)
 {
-    if ((frameNum % 4) == 0) // Change color every 4th frame
-    {
-        s32 intensity = Cos(frameNum, Q_8_8(0.5)) + Q_8_8(0.5);
-        u32 r = 31 - Q_8_8_TO_INT(intensity * 31);
-        u32 g = 31 - Q_8_8_TO_INT(intensity * 22);
-        u32 b = 12;
+//    if ((frameNum % 4) == 0) // Change color every 4th frame
+//    {
+//        s32 intensity = Cos(frameNum, Q_8_8(0.5)) + Q_8_8(0.5);
+//        u32 r = 31 - Q_8_8_TO_INT(intensity * 31);
+//        u32 g = 31 - Q_8_8_TO_INT(intensity * 22);
+//        u32 b = 12;
 
-        u16 color = RGB(r, g, b);
-        LoadPalette(&color, BG_PLTT_ID(14) + 15, sizeof(color));
-   }
+//        u16 color = RGB(r, g, b);
+    u16 color = RGB_BLACK;
+    LoadPalette(&color, BG_PLTT_ID(14) + 15, sizeof(color));
+//   }
 }
